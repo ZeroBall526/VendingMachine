561a58eee48b0c3654bc214290a2f6e8ddca3f9e
ZeroBall526 <96288248+ZeroBall526@users.noreply.github.com>
2025-04-09 00:33:04 +0900

    feat: add coffee integritycheck method

--- src/kr/ac/syu/java/chap4/CoffeeBox.java
+++ src/kr/ac/syu/java/chap4/CoffeeBox.java
@@ -1,10 +1,19 @@
 package kr.ac.syu.java.chap4;
 
+//홍가현 제작
 public class CoffeeBox {
     private int coffeeAmount = 300;
     public boolean useCoffee(int amount) {
         if( coffeeAmount > amount) {
             coffeeAmount -= amount;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean enoughCoffee(int amountTest) {
+        if( coffeeAmount >= amountTest) {
             return true;
         }
         else {
